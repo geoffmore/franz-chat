bbad59dbfe6e25c015548db97f9a15125d9d7141
Jeff Moore <jeoffmore+git@gmail.com>
2024-11-17 01:49:10 -0500

    Add custom Prometheus collector

--- internal/lib/pg_metrics.go
+++ internal/lib/pg_metrics.go
@@ -1,43 +1,94 @@
 package lib
 
 import (
+	"context"
+	"fmt"
+	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/rs/zerolog/log"
 )
 
 // See https://pkg.go.dev/github.com/prometheus/client_golang/prometheus#Registry
 
-type PGMetrics struct {
-	UsersTotal    prometheus.Gauge
-	ChannelsTotal prometheus.Gauge
-	MessagesTotal *prometheus.GaugeVec
+// NOTE - each of Gauge, Counter, Histogram, Summary are all collectors, so they can be registered/unregistered programmatically already
+
+// franzDBStatsCollector inspired by https://github.com/prometheus/client_golang/blob/v1.20.5/prometheus/collectors/dbstats_collector.go#L40
+type franzDBStatsCollector struct {
+	conn          *pgxpool.Conn
+	ctx           context.Context
+	usersTotal    *prometheus.Desc
+	channelsTotal *prometheus.Desc
+	messagesTotal *prometheus.Desc
 }
 
-// Returns metrics only used during session-level advisory lock hold
-func NewPGMetrics(reg prometheus.Registerer) *PGMetrics {
-	// TODO - switch from promauto factory to custom register/unregister
-	factory := promauto.With(reg)
-	// See https://prometheus.io/docs/practices/naming/ for naming
-	return &PGMetrics{
-		UsersTotal: factory.NewGauge(prometheus.GaugeOpts{
-			Name:      "users_total",
-			Help:      "Current users present in app.",
-			Namespace: metricsNamespace,
-		}),
-		ChannelsTotal: factory.NewGauge(prometheus.GaugeOpts{
-			Name: "channels_total",
-			Help: "Current channels present in app.",
-			// TODO - add channel_name, channel_id as labels
-			Namespace: metricsNamespace,
-		}),
-		MessagesTotal: factory.NewGaugeVec(prometheus.GaugeOpts{
-			Name: "messages_total",
-			Help: "Current messages present in app, by channel.",
-			// TODO - add channel_name, channel_id as labels
-			Namespace: metricsNamespace,
-		}, []string{LabelChannelName, LabelChannelID},
+// Collect implements prometheus.Collector
+func (c *franzDBStatsCollector) Collect(ch chan<- prometheus.Metric) {
+	channelMessageCounts := c.getChannelMessagesTotal(c.ctx, c.conn)
+
+	ch <- prometheus.MustNewConstMetric(c.usersTotal, prometheus.GaugeValue, c.getUsersTotal(c.ctx, c.conn))
+	ch <- prometheus.MustNewConstMetric(c.channelsTotal, prometheus.GaugeValue, c.getChannelsTotal(c.ctx, c.conn))
+
+	for k, v := range channelMessageCounts {
+		ch <- prometheus.MustNewConstMetric(c.messagesTotal, prometheus.GaugeValue, v, k)
+	}
+}
+
+// Describe implements prometheus.Collector
+func (c *franzDBStatsCollector) Describe(ch chan<- *prometheus.Desc) {
+	ch <- c.usersTotal
+	ch <- c.channelsTotal
+	ch <- c.messagesTotal
+}
+
+func NewFranzDBStatsCollector(ctx context.Context, conn *pgxpool.Conn) prometheus.Collector {
+	fqName := func(name string) string {
+		return fmt.Sprintf("%s_%s", metricsNamespace, name)
+	}
+	return &franzDBStatsCollector{
+		conn: conn,
+		ctx:  ctx,
+		usersTotal: prometheus.NewDesc(
+			fqName("users_total"),
+			"Current users present in app.",
+			nil, nil,
+		),
+		channelsTotal: prometheus.NewDesc(
+			fqName("channels_total"),
+			"Current channels present in app.",
+			nil, nil,
+		),
+		messagesTotal: prometheus.NewDesc(
+			fqName("messages_total"),
+			"Current messages present in app, by channel.",
+			[]string{LabelChannelID}, nil,
 		),
 	}
-	// reg.Register() w/ err on lock acquisition
-	// reg.Unregister() on lock release
+}
+
+// This generates a prometheus metric of a count of users on the database
+// This remains private because I only want code calling this when the advisory lock is held
+func (c *franzDBStatsCollector) getUsersTotal(ctx context.Context, conn *pgxpool.Conn) float64 {
+	var i int
+	row := conn.QueryRow(ctx, "SELECT count(*) FROM users;")
+
+	if err := row.Scan(&i); HandlePGError(err, nil) != nil {
+		log.Err(err) // TODO - determine how to log this error without embedding a logger in this package
+	}
+	return float64(i)
+}
+
+func (c *franzDBStatsCollector) getChannelsTotal(ctx context.Context, conn *pgxpool.Conn) float64 {
+	var i int
+	row := conn.QueryRow(ctx, "SELECT count(*) FROM channels;")
+
+	if err := row.Scan(&i); HandlePGError(err, nil) != nil {
+		log.Err(err)
+	}
+	return float64(i)
+}
+
+func (c *franzDBStatsCollector) getChannelMessagesTotal(ctx context.Context, conn *pgxpool.Conn) map[string]float64 {
+	_, _ = ctx, conn
+	return map[string]float64{"TODO": 54321}
+	// TODO - do a join and make a curry with prometheus
 }
